4650c7032f0aba763a164c84218e843d3533a6fd
Felix Polo <brunolok@gmail.com>
2024-08-05 14:16:37 -0300

    se creo el bloque de codigo del posteo de las tareas

--- src/controllers/tasks.controllers.js
+++ src/controllers/tasks.controllers.js
@@ -11,3 +11,14 @@ const traerTasks = async (req, res)=>{
 }
 
 module.exports = traerTasks
+
+const crearTareas = async ()=>{
+    try {
+        const {title,description,isComplete} = req.body;
+        const conecction = await coneccion();
+        conecction.query('')
+
+    } catch (error) {
+        
+    }
+}
--- src/routers/tasks.routes.js
+++ src/routers/tasks.routes.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const traerTasks = require('./../controllers/tasks.controllers')
 
 router.get('/tasks', traerTasks);
+router.post('/tasks',)
 
 module.exports = router;
