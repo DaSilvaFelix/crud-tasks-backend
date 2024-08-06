b217615a9da3ae33d417edd828ad3dcddfd8129e
Da Silva Felix Bruno <brunolok79@gmai.com>
2024-08-06 20:46:38 -0300

    se corrigieron los archivos con su respectiva validacion

---
+++ instruciones.md
@@ -0,0 +1,11 @@
+# deben hacer el get en este link:http://localhost:3000/tasks
+
+# si quieres por id es este :http://localhost:3000/tasks/id
+
+# si quieres actualizar debes rellenar todos los campos pero si quieres modificar solo uno debes jear los otros campos con su respectivos datos solo cambia el que quieres actualizar
+
+# si quieres eliminar debes hacer un delete en este link:http://localhost:3000/tasks/id
+
+# si quieres crear un nuevo task debes hacer un post en este link:http://localhost:3000/tasks
+
+# los controles de cada fucnion pueden avisar si no se encuentra la tareas
--- src/controllers/tasks.controllers.js
+++ src/controllers/tasks.controllers.js
@@ -4,7 +4,7 @@ const traerTasks = async (req, res) => {
   try {
     const conecction = await coneccion();
     const tareas = await conecction.query("SELECT * FROM tasks");
-    res.json(tareas[0]);
+    res.status(200).json(tareas[0]);
   } catch (error) {
     console.log(error);
   }
@@ -15,42 +15,81 @@ const crearTareas = async (req, res) => {
     console.log(req.body);
 
     const { title, description, isComplete } = req.body;
-    const conecction = await coneccion();
-    await conecction.query(
-      "INSERT INTO `tasks`( `title`, `description`, `isComplete`) VALUES (?,?,?)",
-      [title, description, isComplete]
-    );
-    res.json({ msg: "tarea creada" });
+    if (!title || !description || !isComplete) {
+      res.json({ msg: "todos los capos deben estar completos" });
+    } else {
+      const conecction = await coneccion();
+      await conecction.query(
+        "INSERT INTO `tasks`( `title`, `description`, `isComplete`) VALUES (?,?,?)",
+        [title, description, isComplete]
+      );
+      res.status(201).json({ msg: "tarea creada" });
+    }
   } catch (error) {
     console.log(error);
   }
 };
 
 const traerTareaPorId = async (req, res) => {
-  const { id } = req.params;
-  const conecction = await coneccion();
-  const searchTasks = await conecction.query(
-    "SELECT * FROM `tasks` WHERE id =?",
-    id
-  );
-  res.json(searchTasks[0]);
+  try {
+    const { id } = req.params;
+    const conecction = await coneccion();
+    const [results] = await conecction.query(
+      "SELECT * FROM `tasks` WHERE id = ?",
+      [id]
+    );
+    const searchTask = results.find((task) => task.id === parseInt(id, 10));
+
+    if (!searchTask) {
+      return res
+        .status(404)
+        .json({ msg: "La tarea buscada no se ha encontrado" });
+    }
+
+    res.json(searchTask);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 const actualizarPorId = async (req, res) => {
-  const { id } = req.params;
-  const { title, description, isComplete } = req.body;
-  const conecction = await coneccion();
-  await conecction.query(
-    "UPDATE `tasks` SET `title`= ?,`description`=?,`isComplete`= ? WHERE id = ?",
-    [title, description, isComplete, id]
-  );
-  res.json({ msg: "tareas actualizada correctamente" });
+  try {
+    const { id } = req.params;
+    const { title, description, isComplete } = req.body;
+
+    const conecction = await coneccion();
+    const [results] = await conecction.query(
+      "SELECT * FROM `tasks` WHERE id = ?",
+      [id]
+    );
+    if (results.length === 0) {
+      return res.status(404).json({ msg: "La tarea no se ha encontrado" });
+    }
+    await conecction.query(
+      "UPDATE `tasks` SET `title`=?,`description`=?,`isComplete`=? WHERE id = ?",
+      [title, description, isComplete, id]
+    );
+    res.status(200).json({ msg: "Tarea actualizada con éxito" });
+  } catch (error) {
+    console.log(error);
+  }
 };
 const eliminarPorId = async (req, res) => {
-  const { id } = req.params;
-  const conecction = await coneccion();
-  await conecction.query("DELETE FROM `tasks` WHERE id = ?", id);
-  res.json({ msg: "registro eliminado correctamente" });
+  try {
+    const { id } = req.params;
+    const conecction = await coneccion();
+    const deleteTasks = await conecction.query(
+      "DELETE FROM `tasks` WHERE id = ?",
+      id
+    );
+    if (id !== deleteTasks) {
+      res
+        .status(400)
+        .json({ msg: "la tarea que buscas para eliminar no se a encontrado " });
+    } else {
+      res.status(200).json({ msg: "registro eliminado correctamente" });
+    }
+  } catch (error) {}
 };
 module.exports = {
   traerTasks,
