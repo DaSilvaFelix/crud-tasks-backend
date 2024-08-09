b5971ee6f3a8ca4cb1e344d5610cbb83045d9877
Felix Polo <brunolok@gmail.com>
2024-08-09 16:01:43 -0300

    de a poco se va armando las validaciones

--- src/validations/validation.js
+++ src/validations/validation.js
@@ -2,6 +2,7 @@ import { body } from "express-validator";
 //title, description, isComplete 
 export const validador = [
     body("title")
-    .isString().withMessage('el campo debe contener solo numeros')
-    .isEmpty().withMessage('el titulo no puede ser vacion')
+    .isString().withMessage('el titulo debe completarce alfanumericos')
+    .notEmpty().withMessage('el titulo no puede ser vacion')
+    .isLength({min:10}).withMessage("el titulo deve ser mayor a 5 caracteres")
 ];
